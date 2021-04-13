6a625abcb6c1ed1a0f4ccde7e2be23cbac81227a
nettivastaava <vaxandst@cubbli20-gold.local>
2021-04-13 15:23:57 +0300

    tehtavat 4-5

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,34 @@
+package statistics.matcher;
+
+public class QueryBuilder {
+    Matcher matcher;
+    
+    public QueryBuilder() {
+        matcher = new All();
+    }
+    
+    public Matcher build() {
+        return matcher;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }
+    
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
+    }
+    
+    public QueryBuilder oneOf(Matcher m1, Matcher m2) {
+        this.matcher = new Or(m1, m2);
+        return this;
+    }
+    
+}
